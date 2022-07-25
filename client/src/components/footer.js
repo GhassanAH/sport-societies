9ede734b271728e31072fedb3c9d5bd168440c98
Ghassan <gassanalhttali@gmail.com>
2022-07-25 11:29:07 +0400

    aa

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -13,17 +13,21 @@ const Footer = () => {
                 <div className="navs">
                     <div className="nav2">
                         <h2>Contact us</h2>
-                        <a href="mailto:sportsocieties@gmail.com" className="link"><h3>sportsocieties@gmail.com</h3></a>
                         <div className="soc">
+                        <a href="mailto:sportsocieties@gmail.com" className="link"><SocialIcon network="email" style={{ height: 25, width: 25, marginRight:5 }} /><h3>sportsocieties@gmail.com</h3></a>
                         <a href="https://www.instagram.com/sportsocieties/" className="link" target="_blank" rel="noreferrer"><SocialIcon network="instagram"  style={{ height: 25, width: 25 , marginRight:5}} /><h3>instagram</h3></a>
                         <a href="https://twitter.com/SocietiesSport/" className="link" target="_blank" rel="noreferrer"><SocialIcon network="twitter" style={{ height: 25, width: 25, marginRight:5 }} /><h3>Twitter</h3></a>
-                        <a href="https://www.youtube.com/channel/UC1IYKQCHHgwaVT-7ov44yxA/videos" className="link" target="_blank" rel="noreferrer"><SocialIcon network="youtube" style={{ height: 25, width: 25, marginRight:5 }} /><h3>YouTube</h3></a>
+
                         </div>
 
                     </div>
                     <div className="nav2">
                         <h2>About us</h2>
-                        <a href="/aboutpage1" className="link"><h3>About The Website</h3></a>
+                        <div className="soc">
+                        <a href="/aboutpage1" className="link"><SocialIcon network="email" style={{ height: 25, width: 25, marginRight:5 }} /><h3>Blog</h3></a>
+                        <a href="https://www.youtube.com/channel/UC1IYKQCHHgwaVT-7ov44yxA/videos" className="link" target="_blank" rel="noreferrer"><SocialIcon network="youtube" style={{ height: 25, width: 25, marginRight:5 }} /><h3>YouTube</h3></a>
+                        <a href="https://www.facebook.com/groups/414100234014071/" className="link" target="_blank" rel="noreferrer"><SocialIcon network="facebook" style={{ height: 25, width: 25, marginRight:5 }} /><h3>Facebook group</h3></a>
+                        </div>
 
                     </div>
   
